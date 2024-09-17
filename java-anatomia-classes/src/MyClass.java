ee27a3786b20788c7f510e47c71250287ac301cd
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2024-09-17 12:44:49 -0300

    Creating methods

--- java-anatomia-classes/src/MyClass.java
+++ java-anatomia-classes/src/MyClass.java
@@ -1,8 +1,20 @@
 public class MyClass {
     public static void main(String[] args) {
-        String myName = "Gabriel Muniz";
-        int age = 26;
-        double height = 1.80;
-        boolean isMarried = true;
+        //Usin types
+        // String myName = "Gabriel Muniz";
+        // int age = 26;
+        // double height = 1.80;
+        // boolean isMarried = true;
+
+        String name = "Gabriel";
+        String surname = "Muniz";
+
+        String fullName = fullName(name, surname);
+
+        System.out.println(fullName);
+    }
+    //Methods
+    public static String fullName(String name, String surname){
+        return "Method result -> " + name.concat(" ").concat(surname);
     }
 }
