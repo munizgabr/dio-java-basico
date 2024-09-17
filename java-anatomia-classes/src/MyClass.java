c9436448ae13c75f5f4b7da6083d6b1d826b1526
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2024-09-17 12:33:01 -0300

    Using types

---
+++ java-anatomia-classes/src/MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String[] args) {
+        String myName = "Gabriel Muniz";
+        int age = 26;
+        double height = 1.80;
+        boolean isMarried = true;
+    }
+}
